5936f5902ed9b244668cc311000720e5b1def08b
Zain Bin Kafeel <zain.kafeel@pixlr.com>
2025-12-16 01:15:20 +0400

    Changes Point System

--- routes/points.route.js
+++ routes/points.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { addPoints } = require("../controllers/points.controllers");
+const { addPoints, getPointsByEmail, getPointsHistoryByEmail } = require("../controllers/points.controllers");
 
 router.post("/add", addPoints);
+
+router.get("/:email", getPointsByEmail);
+// GET /api/points/:email/history
+router.get("/:email/history", getPointsHistoryByEmail);
 
 module.exports = router;
